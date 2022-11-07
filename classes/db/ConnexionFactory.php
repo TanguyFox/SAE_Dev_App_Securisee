758ac8a8b4a237f7031f09798f224e8e3536bcba
Theo.pz <100997816+pellizza1u@users.noreply.github.com>
2022-11-07 15:08:35 +0100

    Action,ConnectionFactory,Dispacher

--- classes/db/ConnexionFactory.php
+++ classes/db/ConnexionFactory.php
@@ -1,6 +1,30 @@
 <?php
 
-class ConnexionFactory
-{
+namespace netvod\db;
+use PDO;
+ConnectionFactory::setConfig( 'db.config.ini' );
 
+class ConnectionFactory{
+    public static $db = null;
+    public static $config =[];
+
+    public static function setConfig($iniFile): void{
+        self::$config = parse_ini_file($iniFile);
+    }
+
+    public static function makeConnection(){
+        if(self::$db === null){
+            $dsn = self::$config['driver'].
+                ':host='.self::$config['host'].
+                ';dbname='.self::$config['dataBase'];
+            self::$db = new PDO($dsn, self::$config['username'], self::$config['password'],[
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_STRINGIFY_FETCHES => false,
+            ]);
+            self::$db->prepare("SET NAMES 'utf8'")->execute();
+        }
+        return self::$db;
+    }
 }
--- classes/dispatch/Dispatcher.php
+++ classes/dispatch/Dispatcher.php
@@ -1,6 +1,67 @@
 <?php
 
+namespace netvod\dispatch;
+
+use Exception;
+use netvod\action\LogoutAction;
+
 class Dispatcher
 {
+    private string $action;
+
+    public function __construct(string $action){
+        if(isset($_SESSION['user'])) {
+            $this->action = $action;
+        }else{
+            $this->action = 'signin';
+        }
+    }
+
+    public function run(): void
+    {
+        switch ($this->action) {
+            case 'signin':
+                $action = new SigninAction();
+                break;
+            case 'add-user':
+                $action = new AddUserAction();
+                break;
+            case 'logout':
+                $action = new LogoutAction();
+                break;
+            default:
+                $action = new DefaultAction();
+                break;
+        }
+        try {
+            $this->renderPage($action->execute());
+        } catch (Exception $e) {
+            $this->renderPage($e->getMessage());
+        }
+    }
+
+    private function renderPage(string $html): void{
+        $content='
+            <!DOCTYPE html>
+            <html lang="fr">
+            <head>
+                <meta charset="UTF-8">
+                <title>NetVOD - '.$_GET['action'].'</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            </head>
+            <body>
+                <style>
+                    body{
+                        text-align: center;
+                    }  
+                </style>
+            ';
+        $content .= $html;
+        if(isset($_SESSION['user'])) {
+            $content .= '<a href="index.php?action=logout" class="btn btn-danger centerFooter">Logout</a>';
+        }
+        $content .= '</body></html>';
+        print($content);
+    }
 
 }
