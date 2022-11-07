63ee0d31d3ba36d742e07bb7e741c15da8f084c3
TanguyFox <tanguyfox@gmail.com>
2022-11-07 16:16:54 +0100

    init User and SignIn Action

---
+++ classes/action/SignInAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace netvod\action;
+
+use netvod\auth\Auth;
+class SignInAction extends Action
+{
+
+    public function execute(): string
+    {
+        $html="";
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $html .= <<<END
+                    Connexion :<br><br>
+                    <form method="post" action="?action=signin">
+                        <label>e-mail <input type="email" name="email"></label><br><br>
+                        <label>Mot de passe : <input type="password" name="passw" value=""> </label>
+                        <button type="submit">Se connecter</button>
+                    </form>
+                    END;
+        } else {
+            $email = filter_var($_POST['email'],FILTER_VALIDATE_EMAIL);
+            $mdp = filter_var($_POST['passw'], FILTER_SANITIZE_SPECIAL_CHARS);
+            Auth::authenticate($email,$mdp);
+            Auth::loadprofile($email);
+            $utilisateur = unserialize($_SESSION['utilisateur']);
+            $html .= "Bienvenue sur NetVod {$utilisateur->prenom} !";
+            $html .= "<a href='?action=acess-profile'> Choississez votre profil</a><ul>";
+            foreach ($utilisateur->profils as $profil){
+                $html = "<li>$profil->nom</li>";
+            }
+            $html.= "</ul>";
+        }
+    }
+}
--- classes/user/User.php
+++ classes/user/User.php
@@ -2,5 +2,17 @@
 
 class User
 {
+    private string $nom;
+    private string $prenom;
+    private string $email;
+    private string $password;
+    private array $profiles;
 
+    public function __construct(string $n, string $p, string $mail, string $pwd, array $profile=[]){
+        $this->nom=$n;
+        $this->prenom=$p;
+        $this->email=$mail;
+        $this->password=$pwd;
+        $this->profiles=$profile;
+    }
 }
