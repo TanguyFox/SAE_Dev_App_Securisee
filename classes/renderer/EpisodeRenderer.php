0a007de2d94b6f2567216b3d02c5afcaaeb5724b
Theo.pz <100997816+pellizza1u@users.noreply.github.com>
2022-11-08 13:04:49 +0100

    début fonctionnalité 4
    Signed-off-by: Theo.pz <100997816+pellizza1u@users.noreply.github.com>

--- classes/renderer/EpisodeRenderer.php
+++ classes/renderer/EpisodeRenderer.php
@@ -2,25 +2,35 @@
 
 namespace netvod\renderer;
 
+use netvod\contenu\serie\episode\Episode;
+
 class EpisodeRenderer implements Renderer
 {
 
-    public function render(int $selector): string
+    private Episode $episode;
+
+    public function __construct(Episode $episode)
     {
-        return $this->rendered = ($selector == Renderer::COMPACT) ? $this->compact(): $this->long();
+        $this->episode = $episode;
     }
 
-    protected function long():String
+    public function render(int $selector): string
+    {
+        return $this->rendered = ($selector == Renderer::COMPACT) ? $this->compact(): $this->rendererLong();
+    }
+
+    protected function rendererLong():String
     {
         return '
         <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="'.$this->serie->image.'" alt="Serie\'s image">
+          <!--TODO A FAIRE MAXIME-->
           <div class="card-body">
-            <h5 class="card-title">'.$this->serie->titre.' <small> '.$this->serie->annee.' </small></h5>
-            <p class="card-text">'.$this->serie->description.'</p>
+            <h5 class="card-title">'.$this->episode->titre.' <small> '.$this->serie->annee.' </small></h5>
+            <p class="card-text">'.$this->episode->resume.'</p>
             <a href="#" class="btn btn-primary">Details</a>
           </div>
         </div>
         ';
     }
+
 }
