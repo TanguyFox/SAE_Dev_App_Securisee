080f250529c2bdd2ee8cef6479b82adcda66aab6
BJulOs <84396590+BJulOs@users.noreply.github.com>
2022-11-09 12:48:58 +0100

    Dispatcher

--- classes/dispatch/Dispatcher.php
+++ classes/dispatch/Dispatcher.php
@@ -4,6 +4,7 @@ namespace netvod\dispatch;
 
 use Exception;
 use netvod\action\AccueilCatalogueAction;
+use netvod\action\AddNoteAction;
 use netvod\action\DefaultAction;
 use netvod\action\DisplayEpisodeDetailsAction;
 use netvod\action\DisplaySerieAction;
@@ -32,6 +33,7 @@ class Dispatcher
             'AccueilCatalogueAction' => new AccueilCatalogueAction(),
             'DisplayEpisodeDetailsAction' => new DisplayEpisodeDetailsAction(),
             'DisplaySerieAction' => new DisplaySerieAction(),
+	        'AddNoteAction' => new AddNoteAction(),
             default => new DefaultAction(),
         };
         try {
@@ -48,6 +50,8 @@ class Dispatcher
             <head>
                 <meta charset="UTF-8">
                 <title>NetVOD - '.$_GET['action'].'</title>
+                <link rel="stylesheet" href="css/style.css">
+                <link src="jquery/jquery-3.6.0.min.js">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
             </head>
             <body>
