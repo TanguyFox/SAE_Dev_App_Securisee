dd055dd31506c865ffcab7c762afc4a2bae97343
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-11-09 15:17:55 +0100

    :flashlight: Correction of Dispatcher redirection

--- classes/dispatch/Dispatcher.php
+++ classes/dispatch/Dispatcher.php
@@ -19,15 +19,19 @@ class Dispatcher
 {
     private string $action;
 
-    public function __construct(string $action){
-        if(isset($_SESSION['user']) or in_array($action, array(null,"signin","register"))) {
-            $this->action = $action;
-        }else{
-            $this->action = "signin";
+    public function __construct(string $action)
+    {
+        $this->action = $action;
+        if (in_array($action, array(null, "signin", "register"))) {
+            if (isset($_SESSION['user']))
+                $this->action = "user-home-page";
+            else
+                $this->action = "signin";
         }
     }
 
-    public function run(): void
+    public
+    function run(): void
     {
         $action = match ($this->action) {
             'signin' => new SigninAction(),
@@ -48,13 +52,15 @@ class Dispatcher
         }
     }
 
-    private function renderPage(string $html): void{
-        $content='
+    private
+    function renderPage(string $html): void
+    {
+        $content = '
             <!DOCTYPE html>
             <html lang="fr">
             <head>
                 <meta charset="UTF-8">
-                <title>NetVOD - '.$_GET['action'].'</title>
+                <title>NetVOD - ' . $_GET['action'] . '</title>
                 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
             </head>
@@ -69,7 +75,7 @@ class Dispatcher
         if ($this->action != "user-home-page") {
             $content .= '<a href="?action=user-home-page" class="btn btn-primary centerFooter">Home</a>';
         }
-        if(isset($_SESSION['user'])) {
+        if (isset($_SESSION['user'])) {
             $content .= '<a href="?action=logout" class="btn btn-danger centerFooter">Logout</a>';
         }
         $content .= '</body></html>';
