3d8fddaacd4341cc2118f24117cb413b65ce6c1e
BJulOs <84396590+BJulOs@users.noreply.github.com>
2022-11-09 13:47:59 +0100

    AddNoteAction

--- classes/dispatch/Dispatcher.php
+++ classes/dispatch/Dispatcher.php
@@ -5,6 +5,7 @@ namespace netvod\dispatch;
 use Exception;
 use netvod\action\AccueilCatalogueAction;
 use netvod\action\AddFavSeriesAction;
+use netvod\action\AddNoteAction;
 use netvod\action\DefaultAction;
 use netvod\action\DisplayEpisodeDetailsAction;
 use netvod\action\DisplaySerieAction;
@@ -34,7 +35,8 @@ class Dispatcher
             'display-serie' => new DisplaySerieAction(),
             'accueil-catalogue' => new AccueilCatalogueAction(),
             'add-fav-series' => new AddFavSeriesAction(),
-            default => new DefaultAction(),
+			'add-note' => new AddNoteAction(),
+            default => new DefaultAction()
         };
         try {
             $this->renderPage($action->execute());
@@ -50,6 +52,7 @@ class Dispatcher
             <head>
                 <meta charset="UTF-8">
                 <title>NetVOD - '.$_GET['action'].'</title>
+                <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
             </head>
             <body>
