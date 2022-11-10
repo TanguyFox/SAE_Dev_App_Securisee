7a65a2aa25f9cb04ecb4e296786910f0773b55c3
BJulOs <84396590+BJulOs@users.noreply.github.com>
2022-11-10 10:05:49 +0100

    re-push

--- classes/dispatch/Dispatcher.php
+++ classes/dispatch/Dispatcher.php
@@ -5,6 +5,7 @@ namespace netvod\dispatch;
 use Exception;
 use netvod\action\AccessAccountAction;
 use netvod\action\AccueilCatalogueAction;
+use netvod\action\AddComAction;
 use netvod\action\AddFavSeriesAction;
 use netvod\action\AddNoteAction;
 use netvod\action\CreateProfilAction;
